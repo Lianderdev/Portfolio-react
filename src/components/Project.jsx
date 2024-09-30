ca65509790f2aaa969bb21d1713be342a00ff9a4
Liander Souza <liandervinicius13@gmail.com>
2024-09-30 11:31:30 -0300

    add: portfolio in projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,13 +8,13 @@ export function Project({ title, link, img, tools }) {
                 {/* <h2 className="text-[1.7rem] capitalize">{title}</h2>
                 <p className="text-[1.6rem] text-zinc-500">{description}</p> */}
                             <a className="w-full rounded-md overflow-hidden" href={link} target="_blank">
-                                <img href={link} className="object-fill rounded-md transform transition-transform duration-500 hover:scale-110" src={img} alt={title} />
+                                <img href={link} className="h-[160px] object-fill rounded-md transform transition-transform duration-500 hover:scale-110" src={img} alt={title} />
                             </a>
-                            <ul className="flex gap-2 flex-wrap mt-4">
+                            {/* <ul className="flex gap-2 flex-wrap mt-6">
                                 {tools.map((tool) =>
                                     <li key={tool} style={{ userSelect: 'none' }} className="px-4 py-1 uppercase border bg-tranparent rounded-md text-xl font-medium text-zinc-50 hover:bg-zinc-900 duration-500 transition-colors">{tool}</li>
                                 )}
-                            </ul>
+                            </ul> */}
             </CardContent>
         </Card>
     )
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,11 +2,13 @@ import spyder_man from '../assets/images/spyder.jpg'
 import burguer_king from '../assets/images/bk.jpg'
 import figpro from '../assets/images/figpro.jpg'
 import todolist from '../assets/images/todolist.jpg'
+import portfolio from '../assets/images/portfolio.jpg'
 
 
 export const projects = [
-    {id:1, title:'Spyder Man 2', description:'lading page', link:'https://spyder-man-lading-page.vercel.app/', img: spyder_man, tools: ['html', 'css', 'javascript']},
-    {id:2, title:'Burguer king - Cupons', description:'lading page', link:'https://bk-cupons-lading-page.vercel.app/', img: burguer_king, tools: ['html', 'css', 'javascript']},
-    {id:3, title:'Figpro', description:'lading page', link:'https://lianderdev.github.io/FigPro-Lading-page/', img: figpro, tools: ['html', 'css', 'javascript']},
-    {id:4, title:'To do list', description:'Site de tarefas', link:'http://todolist-three-hazel.vercel.app/', img: todolist, tools: ['html', 'css', 'javascript']},
+    {id:1, title:'Portfólio', description:'Página Pessoal', link:'https://lianderdev-portfolio.vercel.app/', img: portfolio, tools: ['reactjs', 'vitejs', 'shadcn ui', 'tailwind css']},
+    {id:2, title:'Spyder Man 2', description:'lading page', link:'https://spyder-man-lading-page.vercel.app/', img: spyder_man, tools: ['html', 'css', 'javascript']},
+    {id:3, title:'Burguer king - Cupons', description:'lading page', link:'https://bk-cupons-lading-page.vercel.app/', img: burguer_king, tools: ['html', 'css', 'javascript']},
+    {id:4, title:'Figpro', description:'lading page', link:'https://lianderdev.github.io/FigPro-Lading-page/', img: figpro, tools: ['html', 'css', 'javascript']},
+    {id:5, title:'To do list', description:'Site de tarefas', link:'http://todolist-three-hazel.vercel.app/', img: todolist, tools: ['html', 'css', 'javascript']},
 ]
