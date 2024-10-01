3f3316264e0393d2d45cefe9c5c3c75df80ac7ce
Liander Souza <liandervinicius13@gmail.com>
2024-10-01 00:09:26 -0300

    work: header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,18 +1,32 @@
 import { Button } from '@/components/ui/button';
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png';
 import { FiGithub } from "react-icons/fi";
 import { PiLinkedinLogo } from "react-icons/pi";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { RiCloseLargeLine, RiMenu4Fill } from "react-icons/ri";
 import { MdArrowOutward } from "react-icons/md";
 
 export function Header() {
-
-    const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false);
+    const [isMobile, setIsMobile] = useState(false); // Estado para verificar se é mobile
 
     const handle = () => {
-        setVisible(!visible)
-    }
+        if (isMobile) {
+            setVisible(!visible);
+        }
+    };
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            if (window.innerWidth < 768) {
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
+        };
+
+        checkScreenSize(); 
+    }, []);
 
     return (
         <>
@@ -23,27 +37,37 @@ export function Header() {
                     </div>
 
                     <nav>
-                        <ul className={`absolute top-0 h-[1000px] w-[300px] p-8 flex flex-col md:gap-4 z-30 duration-150 transition-all 
+                        <ul className={`absolute top-0 h-[100vh] w-[300px] p-8 flex flex-col md:gap-4 z-30 duration-150 transition-all 
                         border-r 
-                            ${visible ? 'left-0 bg-zinc-950' : '-left-[302px]'} 
-                            md:relative md:p-0 md:h-auto md:top-0 md:w-auto md:flex-row md:items-center md:left-0 md:border-none
-                          `}>
+                        ${visible ? 'left-0 bg-zinc-950' : '-left-[302px]'} 
+                        md:relative md:p-0 md:h-auto md:top-0 md:w-auto md:flex-row md:items-center md:left-0 md:border-none
+                        `}>
 
                             <div className='md:hidden absolute font-extrabold w-14 h-14 flex justify-between items-center rounded-lg border-2 border-white p-[2px]'>
                                 <img src={logo} alt="logo do liander" />
                             </div>
 
                             <h3 className='md:hidden text-xl font-normal text-zinc-400 mt-20 mb-2 border-t pt-4'>Seções</h3>
+
                             <li><a onClick={handle} className='link' href="#home">Home</a></li>
                             <li><a onClick={handle} className='link' href="#tools">Tecnologias</a></li>
                             <li><a onClick={handle} className='link' href="#projects">Projetos</a></li>
                             <li><a onClick={handle} className='link' href="#education">Formações</a></li>
                             <li><a onClick={handle} className='link' href="#contact">Contatos</a></li>
+
                             <div className='md:hidden flex flex-col gap-4'>
-                                <h3 className='md:hidden text-xl font-normal mt-6 text-zinc-400'>Redes Socias</h3>
+                                <h3 className='md:hidden text-xl font-normal mt-6 text-zinc-400'>Redes Sociais</h3>
                                 <ul className='-mt-2'>
-                                    <li className='link'> <a target='_blank' className='flex justify-between items-center' href="https://github.com/Lianderdev">Linkedin <MdArrowOutward /> </a></li>
-                                    <li className='link'> <a target='_blank' className='flex justify-between items-center' href="https://www.linkedin.com/in/liander-vin%C3%ADcius/">Github <MdArrowOutward /></a></li>
+                                    <li className='link'>
+                                        <a target='_blank' className='flex justify-between items-center' href="https://github.com/Lianderdev">
+                                            Github <MdArrowOutward />
+                                        </a>
+                                    </li>
+                                    <li className='link'>
+                                        <a target='_blank' className='flex justify-between items-center' href="https://www.linkedin.com/in/liander-vin%C3%ADcius/">
+                                            Linkedin <MdArrowOutward />
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                             <Button onClick={handle} variant='secondary' className='absolute top-8 right-8 flex p-2 h-12 w-12 border-[2px] border-zinc-50 text-2xl md:hidden duration-500'>
@@ -53,7 +77,7 @@ export function Header() {
                     </nav>
                 </div>
 
-                <div className={`${visible ? 'hidden' : ''} flex items-center gap-6 transition-opacity duration-500`}>
+                <div className={`${visible ? 'hidden' : 'flex'} flex items-center gap-6 transition-opacity duration-500`}>
                     <div className="flex items-center gap-3 relative">
                         <a href="https://github.com/Lianderdev" target="_blank">
                             <FiGithub className="icons text-5xl" />
@@ -69,7 +93,7 @@ export function Header() {
             </header>
 
             {/* Overlay */}
-            <div onClick={handle} className={` ${visible ? 'fixed' : 'hidden'} md:hidden top-0 left-0 w-full h-full bg-headerColor z-10 duration-500`}></div>
+            <div onClick={handle} className={`${visible ? 'fixed' : 'hidden'} md:hidden top-0 left-0 w-full h-full bg-headerColor z-10 duration-500`}></div>
         </>
-    )
+    );
 }
