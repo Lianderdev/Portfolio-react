c34cb031c4ab1e6eb1689413272b30346c19da12
Liander Souza <liandervinicius13@gmail.com>
2024-10-03 19:59:02 -0300

    styles: project info

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,32 @@
 import { Card, CardContent } from "../components/ui/card"
-import { BsFillInfoSquareFill } from "react-icons/bs";
+import { TfiWorld } from "react-icons/tfi";
 
-export function Project({ title, link, img, tools }) {
+import { Button } from "./ui/button";
+
+export function Project({ title, link, img, tools, description }) {
     return (
         <Card >
-            <CardContent className="flex flex-col p-6 relative overflow-hidden rounded-xl">
-                {/* <h2 className="text-[1.7rem] capitalize">{title}</h2>
-                <p className="text-[1.6rem] text-zinc-500">{description}</p> */}
+            <CardContent className="h-[330px] flex flex-col p-6 relative overflow-hidden rounded-xl">
                 <a className="w-full rounded-lg overflow-hidden" href={link} target="_blank">
                     <img href={link} className=" object-fill transform transition-transform duration-500 hover:scale-110" src={img} alt={title} />
                 </a>
-                {/* <ul className="flex gap-2 flex-wrap mt-6">
+
+                <h2 className="text-[1.7rem] capitalize mt-1">{title}</h2>
+
+                <p className="text-[1.2rem] leading-tight text-zinc-500">{description}</p>
+
+                <ul className="flex gap-2 flex-wrap mt-6">
                     {tools.map((tool) =>
                         <li key={tool} style={{ userSelect: 'none' }} className="px-4 py-1 uppercase border bg-tranparent rounded-md text-xl font-medium text-zinc-50 hover:bg-zinc-900 duration-500 transition-colors">{tool}</li>
                     )}
-                </ul> */}
+                </ul>
 
-                <div className='absolute bottom-4 right-4 bg-zinc-900 backdrop-blur-xl bg-opacity-5 rounded-md p-2 flex items-center justify-center' >
-                    <button className="text-zinc-50 uppercase text-3xl border-none"><BsFillInfoSquareFill/></button>
-                </div>
+                <ul className="absolute right-6 left-6 bottom-6 flex items-center justify-between">
+                    <li><a className="flex items-center gap-1 px-2 py-1 bg-zinc-50 text-xl text-zinc-950 font-semibold rounded-md" href="#"> <TfiWorld className="text-3xl" /> Deploy</a></li>
+                    <li><a href="#">
+                        <Button variant='secondary' className='h-auto flex items-center gap-1 px-2 py-2 text-xl rounded-md'>Código</Button>
+                    </a></li>
+                </ul>
             </CardContent>
         </Card>
     )
